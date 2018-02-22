fe03e14848d5e465f8c77daa1164a1f55f2f5062
Liuxs <lxs1992nmgdx@163.com>
2018-02-22 17:24:47 +0800

    test

--- src/com/liuxs/test3/Hello.java
+++ src/com/liuxs/test3/Hello.java
@@ -2,6 +2,9 @@ package com.liuxs.test3;
 
 public class Hello {
     public static void main(String[] args) {
+        test1();
+    }
+    private static void test1(){
         System.out.println("hello ,IDEA");
     }
 
