56e65a84180fc29ce9b48dde63e17ccd7751cf51
Liuxs <lxs1992nmgdx@163.com>
2018-02-23 10:01:46 +0800

    git change version

--- src/com/liuxs/test3/Hello.java
+++ src/com/liuxs/test3/Hello.java
@@ -3,7 +3,13 @@ package com.liuxs.test3;
 public class Hello {
     public static void main(String[] args) {
         test1();
+        test2();
     }
+
+    private static void test2() {
+        System.out.println("git change Version");
+    }
+
     private static void test1(){
         System.out.println("hello ,IDEA");
     }
