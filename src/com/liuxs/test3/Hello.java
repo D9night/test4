8e0116aea09f6770f5c586437cd9305f14866852
Liuxs <lxs1992nmgdx@163.com>
2018-02-27 16:39:38 +0800

    test

--- src/com/liuxs/test3/Hello.java
+++ src/com/liuxs/test3/Hello.java
@@ -1,11 +1,36 @@
 package com.liuxs.test3;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author lxs
+ */
 public class Hello {
     public static void main(String[] args) {
         test1();
         test2();
         test3();
         test4();
+        test5();
+        test6();
+    }
+
+    private static void test6() {
+        String tab1 = "";
+        System.out.println("tab1 = " + tab1);
+        List<String> a = new ArrayList<>();
+        a.add("1");
+        a.add("2");
+        if (a != null) {
+            for (String s : a) {
+                System.out.println(s);
+            }
+        }
+    }
+
+    private static void test5() {
+        System.out.println("unversioned Files");
     }
 
     private static void test4() {
